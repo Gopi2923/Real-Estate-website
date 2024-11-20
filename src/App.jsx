fcfdd9a83d29a9589aa4864863c37637119ea8bc
Gopi2923 <dgopidhikshan@gmail.com>
2024-11-20 22:57:07 +0530

    added tailwind package.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <h1>Real Estate</h1>
+     <Navbar />
     </div>
   )
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='min-h-screen mb-7 bg-cover bg-center flex items-start w-full overflow-hidden' style={{backgroundImage: "url('./header_img.png')"}}>
+      
+    </div>
+  )
+}
+
+export default Header
